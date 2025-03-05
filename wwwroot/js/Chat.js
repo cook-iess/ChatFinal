6169fa75067ef08259da9d89ebc23c632602d625
CarmelZe <carmel.zerihun@gmail.com>
2025-03-05 14:34:29 +0300

    ChatFinalv3

--- wwwroot/js/Chat.js
+++ wwwroot/js/Chat.js
@@ -2,6 +2,7 @@
 
 var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
 
+
 //Disable the send button until connection is established.
 document.getElementById("sendButton").disabled = true;
 
